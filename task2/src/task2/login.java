4f37ae8c1e3b67d8d324e72ae48fdd7efbb73e6e
nterreri <niccolo.terreri.15@ucl.ac.uk>
2015-10-19 19:24:20 +0100

    Started working on reading login data from file

--- task2/src/task2/login.java
+++ task2/src/task2/login.java
@@ -1,6 +1,15 @@
 package task2;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
 import java.util.Scanner;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.stream.Stream;
 /**
  * Simple console login prompt.
  * <p>
@@ -9,6 +18,8 @@ import java.util.Scanner;
  * Correct details are pre-defined in the source code.
  * <p>
  * TODO: 
+ * Task 0: Attend to TODOS attached to individual method javadocs
+ * <p>
  * TASK 1:
  * place login details in a file, make the program access the file
  * looking first for an account name match, then checking for password
@@ -24,7 +35,7 @@ import java.util.Scanner;
  * </strike>
  * 
  * @author ucabter
- * @version 14/10/15 - 0
+ * @version 19/10/15 - 1
  *
  */
 public class login {
@@ -33,6 +44,7 @@ public class login {
 	static String pw = "4cZpAf";
 
 public static void main(String[] args) {
+	
 		
 		login_prompt();
 		System.out.println("Terminating program.");
@@ -47,6 +59,18 @@ public static void main(String[] args) {
 	 * some login system.
 	 */
 static void login_prompt() {
+
+		String username[] = null, password[] = null;
+		
+		try{
+			
+			fetch_login_data(username, password);
+			
+		} catch (IOException e){
+			System.err.println("Error reading login data file.\n" + 
+					"Terminating program.");
+			System.exit(1);
+		}
 		
 		Scanner is = new Scanner(System.in);
 		
@@ -62,7 +86,8 @@ static void login_prompt() {
 			System.out.println("Please enter your username and password.");
 			System.out.println();
 			System.out.print("Username:");
-			account_in = is.nextLine();
+			account_in = is.next();
+			
 			if(!account.equalsIgnoreCase(account_in))
 			{
 				System.out.println("Account name not recognized.");
@@ -96,4 +121,28 @@ static void login_prompt() {
 		is.close();
 	}
 
+/**TODO: Read login_data until eof and store usernames and passwords in two 
+ * arrays, matching the position of each by array index.
+ * @throws IOException 
+ */
+static void fetch_login_data(String[] username, String[] password) throws IOException{
+	
+	
+	username = new String[10];
+	password = new String[10];
+	int index = 0;
+	
+	BufferedReader is = new BufferedReader(new FileReader("login_data"));
+	
+	while(is.ready())
+	{
+		String nextLine = is.readLine();
+		username[index]= nextLine.substring(11);
+		nextLine = is.readLine();
+		password[index] = nextLine.substring(9);
+		
+	}
+	
+	
+}
 }
